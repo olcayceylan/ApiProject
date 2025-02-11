f8fd5cb1e58baa1886b9cc50a31caf9925f567b7
olcayceylan <ceylanolcay97@gmail.com>
2025-02-11 22:34:47 +0300

    Add project files.

---
+++ WebApiProject/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProject.Context;
+using WebApiProject.Entities;
+
+namespace WebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet] // Listeleme işlemleri için kullanılan bir attributedir.
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList(); //tolist ile verilerimizi getiriyoruz 
+            return Ok(values);
+        }
+
+        [HttpPost] // ekleme işlemleri için kullanılan bir attributedir.
+        public IActionResult CreateCategory(Category category)
+        {                                                               //Swagger Aracı ile ekleme işlemini yapmış olduk.
+            _context.Categories.Add(category);                          
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başırılı");
+        }
+        [HttpDelete] //Silme işlemleri için kullanılan bir attributedir.
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id); // id den gelen değeri bul.
+            _context.Categories.Remove(value); // valueden gelen değeri sil.
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı");
+        }
+        [HttpGet("GetCategory")] //ID ' ye göre getirme işlemi. // aynı attribute kullanılmaz bundan dolayı bir isim eklenir.
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme başarılı");
+        }
+
+    }
+}
---
+++ WebApiProject/Controllers/ChefsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProject.Context;
+using WebApiProject.Entities;
+
+namespace WebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Şef sisteme başarıyla eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Şef Sistemden Silindi");
+        }
+        [HttpGet("GetChef")]
+
+        public IActionResult GetChef(int id)
+        {
+            return Ok(_context.Chefs.Find(id)); //Tek satırda getirmiş oldum.
+        }
+
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef güncelleme işlemi başarılı");
+        }
+    }
+}
